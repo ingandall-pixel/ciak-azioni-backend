fa261edb67f9354bcc68c74ce09d37c8acff8943
ingandall-pixel <ingandall@gmail.com>
2026-08-18 10:33:15 +0200

    Update update_db.py

--- update_db.py
+++ update_db.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 import os
 import time
+import io
 import urllib.request
 from datetime import datetime
 
@@ -30,13 +31,13 @@ def clean_ticker(symbol):
 
 def get_all_tickers():
     update_progress(2, "Caricamento elenchi completi USA ed Italia...")
-    
-    # 1. MERCATO USA COMPLETO (SEC Registro Ufficiale)
     tickers_us = []
+    
+    # 1. MERCATO USA COMPLETO (SEC Registro Ufficiale con User-Agent conforme)
     try:
         req = urllib.request.Request(
             'https://www.sec.gov/files/company_tickers.json',
-            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
+            headers={'User-Agent': 'FinancialAppUser admin@financialapp.com'}
         )
         with urllib.request.urlopen(req) as response:
             sec_data = json.loads(response.read().decode())
@@ -47,6 +48,7 @@ def get_all_tickers():
     except Exception as e:
         log_error(f"Errore recupero SEC USA: {e}")
 
+    # Fallback Wikipedia S&P 500 se SEC fallisce
     if not tickers_us:
         try:
             url_sp500 = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
@@ -55,106 +57,34 @@ def get_all_tickers():
                 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
             )
             with urllib.request.urlopen(req) as response:
-                sp500_df = pd.read_html(response)[0]
+                html = response.read().decode('utf-8')
+                sp500_df = pd.read_html(io.StringIO(html))[0]
                 tickers_us = [clean_ticker(t).replace('.', '-') for t in sp500_df['Symbol'].tolist()]
         except Exception as e:
             log_error(f"Errore recupero Wikipedia S&P500: {e}")
             tickers_us = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'NVDA', 'META', 'TSLA']
 
-    # 2. MERCATO ITALIA COMPLETO (~380+ Titoli Borsa Italiana)
+    # 2. MERCATO ITALIA (Titoli principali Borsa Italiana / FTSE MIB / MidCap / Star)
     tickers_it_raw = [
-        'A2A.MI', 'ACE.MI', 'ACKR.MI', 'ADB.MI', 'AEF.MI', 'AER.MI', 'AGL.MI', 'AIM.MI', 'AJO.MI', 'ALA.MI', 
-        'ALB.MI', 'ALG.MI', 'ALM.MI', 'ALT.MI', 'AMP.MI', 'ANIM.MI', 'ANTM.MI', 'AP.MI', 'ARN.MI', 'ARO.MI', 
-        'ARR.MI', 'ASR.MI', 'AST.MI', 'ATC.MI', 'ATN.MI', 'AUT.MI', 'AZM.MI', 'B3.MI', 'BAC.MI', 'BFF.MI', 
-        'BFR.MI', 'BFG.MI', 'BGN.MI', 'BIA.MI', 'BIB.MI', 'BIG.MI', 'BIO.MI', 'BIT.MI', 'BJ.MI', 'BLI.MI', 
-        'BM.MI', 'BMC.MI', 'BMED.MI', 'BMI.MI', 'BPE.MI', 'BPL.MI', 'BRE.MI', 'BRI.MI', 'BRN.MI', 'BSP.MI', 
-        'BSS.MI', 'BST.MI', 'BSU.MI', 'BVA.MI', 'BVT.MI', 'BZU.MI', 'CAI.MI', 'CALL.MI', 'CARR.MI', 'CAS.MI', 
-        'CAT.MI', 'CBK.MI', 'CBM.MI', 'CBP.MI', 'CBR.MI', 'CBS.MI', 'CE.MI', 'CED.MI', 'CEM.MI', 'CES.MI', 
-        'CFI.MI', 'CFN.MI', 'CG.MI', 'CGF.MI', 'CIA.MI', 'CL.MI', 'CLC.MI', 'CLE.MI', 'CLF.MI', 'CM.MI', 
-        'CMC.MI', 'CNHI.MI', 'COF.MI', 'COG.MI', 'COM.MI', 'CON.MI', 'COP.MI', 'COV.MI', 'CPR.MI', 'CPS.MI', 
-        'CR.MI', 'CRC.MI', 'CRG.MI', 'CRI.MI', 'CRS.MI', 'CRT.MI', 'CS.MI', 'CSF.MI', 'CSM.MI', 'CST.MI', 
-        'CT.MI', 'CTI.MI', 'CTR.MI', 'CUS.MI', 'CV.MI', 'CWC.MI', 'CY.MI', 'DAN.MI', 'DAT.MI', 'DBA.MI', 
-        'DD.MI', 'DEL.MI', 'DIA.MI', 'DIG.MI', 'DIS.MI', 'DLC.MI', 'DLP.MI', 'DM.MI', 'DMM.MI', 'DMT.MI', 
-        'DNC.MI', 'DOP.MI', 'DOS.MI', 'DPS.MI', 'DR.MI', 'DRE.MI', 'DRN.MI', 'DS.MI', 'DSM.MI', 'DST.MI', 
-        'E2E.MI', 'EAU.MI', 'EB.MI', 'EBS.MI', 'EC.MI', 'ED.MI', 'EDN.MI', 'EE.MI', 'EEL.MI', 'EFP.MI', 
-        'EGF.MI', 'EGL.MI', 'EGP.MI', 'EG.MI', 'EHP.MI', 'EIP.MI', 'EIR.MI', 'EL.MI', 'ELC.MI', 'ELN.MI', 
-        'EM.MI', 'EMA.MI', 'EMC.MI', 'ENE.MI', 'ENEL.MI', 'ENG.MI', 'ENI.MI', 'ENR.MI', 'ENT.MI', 'EO.MI', 
-        'EOS.MI', 'EP.MI', 'EPR.MI', 'EQU.MI', 'ERA.MI', 'ERG.MI', 'ERI.MI', 'ES.MI', 'ESM.MI', 'EST.MI', 
-        'ETR.MI', 'EUC.MI', 'EUR.MI', 'EUS.MI', 'EVA.MI', 'EVO.MI', 'EVR.MI', 'EXO.MI', 'EXP.MI', 'EZ.MI', 
-        'FAB.MI', 'FAL.MI', 'FAM.MI', 'FAR.MI', 'FAS.MI', 'FAT.MI', 'FAV.MI', 'FB.MI', 'FBO.MI', 'FCD.MI', 
-        'FCL.MI', 'FCT.MI', 'FD.MI', 'FDI.MI', 'FED.MI', 'FEM.MI', 'FER.MI', 'FFD.MI', 'FFI.MI', 'FG.MI', 
-        'FH.MI', 'FHC.MI', 'FI.MI', 'FID.MI', 'FIG.MI', 'FIL.MI', 'FIM.MI', 'FIN.MI', 'FIR.MI', 'FIS.MI', 
-        'FIT.MI', 'FIV.MI', 'FK.MI', 'FKO.MI', 'FLC.MI', 'FLD.MI', 'FLM.MI', 'FLO.MI', 'FLR.MI', 'FLS.MI', 
-        'FLT.MI', 'FLY.MI', 'FMC.MI', 'FME.MI', 'FMI.MI', 'FML.MI', 'FMM.MI', 'FMN.MI', 'FMP.MI', 'FMS.MI', 
-        'FMT.MI', 'FNC.MI', 'FND.MI', 'FNE.MI', 'FNG.MI', 'FNI.MI', 'FNM.MI', 'FNN.MI', 'FNP.MI', 'FNS.MI', 
-        'FNT.MI', 'FNV.MI', 'G.MI', 'GAB.MI', 'GAL.MI', 'GAM.MI', 'GAR.MI', 'GBL.MI', 'GC.MI', 'GCC.MI', 
-        'GDF.MI', 'GEC.MI', 'GEM.MI', 'GEN.MI', 'GEO.MI', 'GER.MI', 'GET.MI', 'GFC.MI', 'GHC.MI', 'GIA.MI', 
-        'GIC.MI', 'GII.MI', 'GIL.MI', 'GIM.MI', 'GIN.MI', 'GIO.MI', 'GIP.MI', 'GIS.MI', 'GIV.MI', 'GLB.MI', 
-        'GMA.MI', 'GMC.MI', 'GME.MI', 'GMI.MI', 'GMM.MI', 'GNA.MI', 'GNC.MI', 'GND.MI', 'GNG.MI', 'GNI.MI', 
-        'GNL.MI', 'GNN.MI', 'GNS.MI', 'GOB.MI', 'GOC.MI', 'GOD.MI', 'GOE.MI', 'GOF.MI', 'GOG.MI', 'GOH.MI', 
-        'GOL.MI', 'GOM.MI', 'GON.MI', 'GOP.MI', 'GOR.MI', 'GOS.MI', 'GOT.MI', 'GPA.MI', 'GPB.MI', 'GPC.MI', 
-        'GPE.MI', 'GPI.MI', 'GPL.MI', 'GPM.MI', 'GPO.MI', 'GPP.MI', 'GPR.MI', 'GPS.MI', 'GPT.MI', 'GPV.MI', 
-        'HER.MI', 'I2A.MI', 'IB.MI', 'IBS.MI', 'IC.MI', 'ICE.MI', 'ICP.MI', 'ID.MI', 'IDC.MI', 'IDE.MI', 
-        'IDR.MI', 'IDS.MI', 'IE.MI', 'IF.MI', 'IFC.MI', 'IFF.MI', 'IFI.MI', 'IG.MI', 'IGD.MI', 'IGE.MI', 
-        'III.MI', 'IJE.MI', 'IK.MI', 'IL.MI', 'ILC.MI', 'ILL.MI', 'ILM.MI', 'IM.MI', 'IMA.MI', 'IMC.MI', 
-        'IMD.MI', 'IMF.MI', 'IMI.MI', 'IN.MI', 'INA.MI', 'INB.MI', 'INC.MI', 'IND.MI', 'INE.MI', 'INF.MI', 
-        'ING.MI', 'INI.MI', 'INM.MI', 'INP.MI', 'INR.MI', 'INS.MI', 'INT.MI', 'INW.MI', 'IP.MI', 'IPC.MI', 
-        'IPE.MI', 'IPM.MI', 'IPO.MI', 'IPR.MI', 'IPS.MI', 'IQ.MI', 'IR.MI', 'IRA.MI', 'IRC.MI', 'IRE.MI', 
-        'IRI.MI', 'IRM.MI', 'IRR.MI', 'IRS.MI', 'IS.MI', 'ISC.MI', 'ISI.MI', 'ISM.MI', 'ISP.MI', 'ISS.MI', 
-        'IST.MI', 'IT.MI', 'ITA.MI', 'ITC.MI', 'ITE.MI', 'ITG.MI', 'ITI.MI', 'ITM.MI', 'ITO.MI', 'ITS.MI', 
-        'IV.MI', 'IVG.MI', 'IVS.MI', 'IZ.MI', 'JUVE.MI', 'K.MI', 'KB.MI', 'KC.MI', 'KE.MI', 'KF.MI', 
-        'KG.MI', 'KIP.MI', 'KM.MI', 'KN.MI', 'KOP.MI', 'KR.MI', 'KS.MI', 'L.MI', 'LA.MI', 'LAB.MI', 
-        'LAC.MI', 'LAD.MI', 'LDO.MI', 'LEA.MI', 'LEC.MI', 'LEG.MI', 'LEO.MI', 'LES.MI', 'LGT.MI', 'LI.MI', 
-        'LIC.MI', 'LID.MI', 'LIE.MI', 'LIF.MI', 'LIG.MI', 'LII.MI', 'LIM.MI', 'LIN.MI', 'LIP.MI', 'LIS.MI', 
-        'LIT.MI', 'LL.MI', 'LM.MI', 'LMC.MI', 'LMI.MI', 'LMM.MI', 'LMS.MI', 'LN.MI', 'LOC.MI', 'LOG.MI', 
-        'LOM.MI', 'LON.MI', 'LOP.MI', 'LOR.MI', 'LTA.MI', 'LTC.MI', 'LTI.MI', 'LU.MI', 'LUB.MI', 'LUC.MI', 
-        'LUD.MI', 'LUM.MI', 'LUP.MI', 'LUS.MI', 'LUT.MI', 'LUVE.MI', 'LV.MI', 'LVM.MI', 'LY.MI', 'M3.MI', 
-        'MA.MI', 'MAB.MI', 'MAC.MI', 'MAD.MI', 'MAE.MI', 'MAF.MI', 'MAG.MI', 'MAI.MI', 'MAL.MI', 'MAM.MI', 
-        'MAP.MI', 'MAR.MI', 'MAS.MI', 'MAT.MI', 'MB.MI', 'MBA.MI', 'MBF.MI', 'MBI.MI', 'MBM.MI', 'MBS.MI', 
-        'MC.MI', 'MCA.MI', 'MCB.MI', 'MCC.MI', 'MCD.MI', 'MCE.MI', 'MCF.MI', 'MCG.MI', 'MCH.MI', 'MCI.MI', 
-        'MCL.MI', 'MCM.MI', 'MCN.MI', 'MCO.MI', 'MCP.MI', 'MCR.MI', 'MCS.MI', 'MCT.MI', 'MD.MI', 'MDA.MI', 
-        'MDB.MI', 'MDC.MI', 'MDF.MI', 'MDG.MI', 'MDI.MI', 'MDL.MI', 'MDM.MI', 'MDN.MI', 'MDP.MI', 'MDR.MI', 
-        'MDS.MI', 'MDT.MI', 'ME.MI', 'MEA.MI', 'MEB.MI', 'MEC.MI', 'MED.MI', 'MEE.MI', 'MEG.MI', 'MEI.MI', 
-        'MEL.MI', 'MEM.MI', 'MEN.MI', 'MER.MI', 'MES.MI', 'MET.MI', 'MFA.MI', 'MFB.MI', 'MFC.MI', 'MFEA.MI', 
-        'MFEB.MI', 'MFG.MI', 'MFI.MI', 'MFL.MI', 'MFM.MI', 'MFN.MI', 'MFR.MI', 'MFS.MI', 'MG.MI', 'MGA.MI', 
-        'MGB.MI', 'MGC.MI', 'MGD.MI', 'MGE.MI', 'MGF.MI', 'MGI.MI', 'MGL.MI', 'MGM.MI', 'MGN.MI', 'MGO.MI', 
-        'MGP.MI', 'MGR.MI', 'MGS.MI', 'MH.MI', 'MHA.MI', 'MHB.MI', 'MHC.MI', 'MHD.MI', 'MHE.MI', 'MHF.MI', 
-        'MHI.MI', 'MHL.MI', 'MHM.MI', 'MHN.MI', 'MHP.MI', 'MHR.MI', 'MHS.MI', 'MI.MI', 'MIA.MI', 'MIB.MI', 
-        'MIC.MI', 'MID.MI', 'MIE.MI', 'MIF.MI', 'MIG.MI', 'MIL.MI', 'MIM.MI', 'MIN.MI', 'MIP.MI', 'MIR.MI', 
-        'MIS.MI', 'MIT.MI', 'MIV.MI', 'MJ.MI', 'MK.MI', 'MKA.MI', 'MKB.MI', 'MKC.MI', 'MKE.MI', 'MKL.MI', 
-        'MKM.MI', 'ML.MI', 'MLA.MI', 'MLB.MI', 'MLC.MI', 'MLE.MI', 'MLF.MI', 'MLG.MI', 'MLI.MI', 'MLL.MI', 
-        'MLM.MI', 'MLN.MI', 'MLP.MI', 'MLR.MI', 'MLS.MI', 'MLT.MI', 'MM.MI', 'MMA.MI', 'MMB.MI', 'MMC.MI', 
-        'MMD.MI', 'MME.MI', 'MMF.MI', 'MMG.MI', 'MMI.MI', 'MML.MI', 'MMM.MI', 'MMN.MI', 'MMP.MI', 'MMR.MI', 
-        'MMS.MI', 'MMT.MI', 'MN.MI', 'MNA.MI', 'MNB.MI', 'MNC.MI', 'MND.MI', 'MNE.MI', 'MNF.MI', 'MNG.MI', 
-        'MNI.MI', 'MNL.MI', 'MNM.MI', 'MNN.MI', 'MNP.MI', 'MNR.MI', 'MNS.MI', 'MNT.MI', 'MO.MI', 'MOA.MI', 
-        'MOB.MI', 'MOC.MI', 'MOD.MI', 'MOE.MI', 'MOF.MI', 'MOG.MI', 'MOI.MI', 'MOL.MI', 'MOM.MI', 'MON.MI', 
-        'MONC.MI', 'MOP.MI', 'MOR.MI', 'MOS.MI', 'MOT.MI', 'MP.MI', 'MPA.MI', 'MPB.MI', 'MPC.MI', 'MPE.MI', 
-        'MPF.MI', 'MPG.MI', 'MPI.MI', 'MPL.MI', 'MPM.MI', 'MPN.MI', 'MPO.MI', 'MPR.MI', 'MPS.MI', 'MPT.MI', 
-        'MR.MI', 'MRA.MI', 'MRB.MI', 'MRC.MI', 'MRD.MI', 'MRE.MI', 'MRF.MI', 'MRG.MI', 'MRI.MI', 'MRL.MI', 
-        'MRM.MI', 'MRN.MI', 'MRO.MI', 'MRP.MI', 'MRR.MI', 'MRS.MI', 'MRT.MI', 'MS.MI', 'MSA.MI', 'MSB.MI', 
-        'MSC.MI', 'MSD.MI', 'MSE.MI', 'MSF.MI', 'MSG.MI', 'MSI.MI', 'MSL.MI', 'MSM.MI', 'MSN.MI', 'MSO.MI', 
-        'MSP.MI', 'MSR.MI', 'MSS.MI', 'MST.MI', 'MT.MI', 'MTA.MI', 'MTB.MI', 'MTC.MI', 'MTD.MI', 'MTE.MI', 
-        'MTF.MI', 'MTG.MI', 'MTI.MI', 'MTL.MI', 'MTM.MI', 'MTN.MI', 'MTO.MI', 'MTP.MI', 'MTR.MI', 'MTS.MI', 
-        'MTT.MI', 'MU.MI', 'MUA.MI', 'MUB.MI', 'MUC.MI', 'MUD.MI', 'MUE.MI', 'MUF.MI', 'MUG.MI', 'MUI.MI', 
-        'MUL.MI', 'MUM.MI', 'MUN.MI', 'MUP.MI', 'MUR.MI', 'MUS.MI', 'MUT.MI', 'MV.MI', 'MVA.MI', 'MVB.MI', 
-        'MVC.MI', 'MVE.MI', 'MVF.MI', 'MVG.MI', 'MVI.MI', 'MVL.MI', 'MVM.MI', 'MVN.MI', 'MVP.MI', 'MVR.MI', 
-        'MVS.MI', 'MVT.MI', 'MW.MI', 'MX.MI', 'MY.MI', 'MZ.MI', 'NEXI.MI', 'O2A.MI', 'PIA.MI', 'PRY.MI', 
-        'PST.MI', 'RACE.MI', 'REC.MI', 'RWAY.MI', 'SAF.MI', 'SFL.MI', 'SL.MI', 'SPM.MI', 'SRG.MI', 'STM.MI', 
-        'TEN.MI', 'TIS.MI', 'TIT.MI', 'TOD.MI', 'TRN.MI', 'TXT.MI', 'UCG.MI', 'UNI.MI', 'US.MI', 'VLS.MI', 
-        'VTY.MI', 'WBA.MI', 'ZUC.MI'
+        'A2A.MI', 'ACE.MI', 'AMP.MI', 'ANIM.MI', 'ARN.MI', 'AZM.MI', 'B3.MI', 'BAMI.MI', 
+        'BFF.MI', 'BGN.MI', 'BMED.MI', 'BPE.MI', 'BRE.MI', 'BZU.MI', 'CPR.MI', 'DIA.MI', 
+        'ELN.MI', 'ENEL.MI', 'ENI.MI', 'ERG.MI', 'EUC.MI', 'FBK.MI', 'FCT.MI', 'G.MI', 
+        'GHC.MI', 'IGD.MI', 'INW.MI', 'IP.MI', 'ISP.MI', 'IVG.MI', 'JUVE.MI', 'LDO.MI', 
+        'LUVE.MI', 'MB.MI', 'MFEA.MI', 'MFEB.MI', 'MONC.MI', 'NEXI.MI', 'PIA.MI', 'PIR.MI', 
+        'PRY.MI', 'PST.MI', 'RACE.MI', 'REC.MI', 'RWAY.MI', 'SAF.MI', 'SFL.MI', 'SL.MI', 
+        'SPM.MI', 'SRG.MI', 'STM.MI', 'TEN.MI', 'TIT.MI', 'TRN.MI', 'TXT.MI', 'UCG.MI', 
+        'UNI.MI', 'VTY.MI', 'WBA.MI'
     ]
 
     tickers_it = [clean_ticker(t) for t in tickers_it_raw if clean_ticker(t)]
-
-    all_cleaned = list(dict.fromkeys(tickers_it + tickers_us))
-    return all_cleaned
+    return list(dict.fromkeys(tickers_it + tickers_us))
 
 def download_data():
     db_exists = os.path.exists(DB_FILE)
     market_data = {}
 
     if db_exists:
-        update_progress(5, "Database trovato. Aggiornamento incrementale a blocchi...")
+        update_progress(5, "Database trovato. Aggiornamento incrementale...")
         try:
             with open(DB_FILE, 'r') as f:
                 market_data = json.load(f)
@@ -163,7 +93,7 @@ def download_data():
             market_data = {}
         period_to_fetch = "5d"
     else:
-        update_progress(5, "Database assente. Download massivo a blocchi...")
+        update_progress(5, "Database assente. Scaricamento completo...")
         period_to_fetch = "5y"
 
     ALL_TICKERS = get_all_tickers()
@@ -236,7 +166,7 @@ def download_data():
             time.sleep(1.0)
             continue
 
-    # Calcolo statistiche finali per il report pop-up
+    # Statistiche finali per il pop-up report
     it_count = 0
     us_count = 0
     it_years = []
